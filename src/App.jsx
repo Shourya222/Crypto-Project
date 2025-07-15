216cfda8aea4796828d160a418b45c69eb20f442
shourya <shouryaverma222@gmail.com>
2025-07-15 19:26:31 +0530

    Added react query config

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css'
 import Banner from './Components/Banner/Banner'
-import CoinTable from './Components/CoinTable'
+import CoinTable from './Components/CoinTable/CoinTable'
 import Navbar from './Components/Navbar/Navbar'
 
 function App() {
