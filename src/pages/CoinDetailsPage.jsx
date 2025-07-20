8ffdad3f14d82c10868b74d4329b82a03c15fded
shourya <shouryaverma222@gmail.com>
2025-07-20 22:43:05 +0530

    Started consuming coin details api

--- src/pages/CoinDetailsPage.jsx
+++ src/pages/CoinDetailsPage.jsx
@@ -1,13 +1,73 @@
+import { useQuery } from "@tanstack/react-query";
+import parse from 'html-react-parser';
+import { useContext, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import { fetchCoinDetails } from "../services/fetchCoinDetails";
+import { CurrencyContext } from "../context/CurrencyContext";
 
-function CoinDetailsPage(){
+function CoinDetailsPage() {
+  const { coinId } = useParams();
+  const {currency} = useContext(CurrencyContext)
+  
 
-    const {coinId} = useParams();
-    return(
-        <div>
-            <h1>Coin Details Page {coinId}</h1>
+  const {
+    isError,
+    isLoading,
+    data,
+  } = useQuery({
+    queryKey: ["coins", coinId],
+    queryFn: () => fetchCoinDetails(coinId),
+    cacheTime: 1000 * 60 * 2,
+    staleTime: 100 * 60 * 2,
+  });
+
+  useEffect(()=>{
+    console.log(data);
+  },[data])
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (isError) {
+    return <div>Error: something went wrong</div>;
+  }
+
+  return (
+        
+    <div className="flex flex-col md:flex-row">
+      <div className="md:w-1/3 w-full flex flex-col items-center mt-6 md:mt-0 border-r-2 border-gray-500">
+        <img alt={data?.name} src={data?.image?.large} className="h-52 mb-5" />
+        <h1 className="text-4xl font-bold ">
+            {data?.name}
+        </h1>
+        <p className="w-full px-6 py-4 text-justify"
+        >
+            {parse(data?.description?.en)}
+        </p>
+        <div 
+            className="w-full flex flex-col md:flex-row md:justify-around"
+        >
+            <div
+                className="flex items-center mb-4 md:mb-0"
+            >
+                <h2 className="text-xl font-bold">Rank</h2>
+                <span className="ml-3 text-xl">{data?.market_cap_rank}</span>
+            </div>
+            <div
+                className="flex items-center mb-4 md:mb-0"
+            >
+                <h2 className="text-xl font-bold">Current Price</h2>
+                <span className="ml-3 text-xl">{data?.market_data.current_price[currency]}</span>
+            </div>
+
         </div>
-    )
+      </div>
+
+      <div className="md:2/3 w-full p-6"></div>
+        
+    </div>
+  );
 }
 
 export default CoinDetailsPage;
